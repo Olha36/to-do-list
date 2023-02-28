1eb5a3f77dc935daff63028455354ff5ea36b50c
Olha36 <olhakucheruk36@gmail.com>
2023-02-28 21:56:55 +0200

    correction: layout

--- src/components/ToDoList/index.tsx
+++ src/components/ToDoList/index.tsx
@@ -3,16 +3,18 @@ import React from 'react'
 const ToDoList = () => {
   return (
     <div className="App">
-      <h1>To Do list</h1>
-      <div>
-        <h5>Title:</h5>
-        <input type="text" placeholder='enter title' required/>
+      <h1> &lsquo; To Do &rsquo; list</h1>
+      <div className="parent" style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        <div>
+          <h5>Title:</h5>
+          <input type="text" placeholder="enter title" required />
+        </div>
+        <div>
+          <h5>Description:</h5>
+          <input type="text" placeholder="enter description" required />
+        </div>
+        <button style={{ width: 177, height: 21, alignSelf: 'flex-end' }}>Create</button>
       </div>
-      <div>
-        <h5>Description:</h5>
-        <input type="text" placeholder='enter description' required/>
-      </div>
-      <button>Create</button>
     </div>
   )
 }
