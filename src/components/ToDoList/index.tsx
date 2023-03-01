f45642b6293d7afdbad3b46856d6dd2e8a8e4e98
Olha36 <olhakucheruk36@gmail.com>
2023-03-01 12:09:17 +0200

    add: function to create a new To-Do item

--- src/components/ToDoList/index.tsx
+++ src/components/ToDoList/index.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
 
+export type ToDo = {
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+}
+
 const ToDoList: React.FC = () => {
     const [toDoList, setToDoList] = useState<ToDo[]>([])
     const [titleInput, setTitleInput] = useState('');
@@ -12,6 +19,17 @@ const ToDoList: React.FC = () => {
         const handleDescriptionInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setDescriptionInput(event.target.value);
     };
+    const handleCreateToDo = () => {
+        const newToDo: ToDo = {
+          id: toDoList.length + 1,
+          title: titleInput,
+          description: descriptionInput,
+          status: 'in progress',
+        };
+        setToDoList([...toDoList, newToDo]);
+        setTitleInput('');
+        setDescriptionInput('');
+      };
 
   return (
     <div className="App">
@@ -19,16 +37,16 @@ const ToDoList: React.FC = () => {
       <div className="parent" style={{ display: 'flex', justifyContent: 'space-evenly' }}>
         <div>
           <h5>Title:</h5>
-          <input type="text" placeholder="enter title" required />
+          <input type="text" value={titleInput} onChange={handleTitleInputChange} placeholder="enter title" required />
         </div>
         <div>
           <h5>Description:</h5>
-          <input type="text" placeholder="enter description" required />
+          <input type="text" value={descriptionInput} onChange={handleDescriptionInputChange} placeholder="enter description" required />
         </div>
-        <button style={{ width: 177, height: 21, alignSelf: 'flex-end' }} >Create</button>
+        <button style={{ width: 177, height: 21, alignSelf: 'flex-end' }} onClick={handleCreateToDo}>Create</button>
       </div>
     </div>
   )
 }
-export default ToDoList
+export default ToDoList;
 
