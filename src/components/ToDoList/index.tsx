7469aeeef66d232719e5feddcfdc9b255e1a8db9
Olha36 <olhakucheruk36@gmail.com>
2023-03-01 13:39:44 +0200

    finished: ToDoList

--- src/components/ToDoList/index.tsx
+++ src/components/ToDoList/index.tsx
@@ -1,35 +1,48 @@
 import React, { useState } from 'react'
 
 export type ToDo = {
-  id: number;
-  title: string;
-  description: string;
-  status: string;
+  id: number
+  title: string
+  description: string
+  status: string
 }
 
 const ToDoList: React.FC = () => {
-    const [toDoList, setToDoList] = useState<ToDo[]>([])
-    const [titleInput, setTitleInput] = useState('');
-    const [descriptionInput, setDescriptionInput] = useState('');
+  const [toDoList, setToDoList] = useState<ToDo[]>([])
+  const [titleInput, setTitleInput] = useState('')
+  const [descriptionInput, setDescriptionInput] = useState('')
+  const [error, setError] = useState(false)
 
-    const handleTitleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTitleInput(event.target.value);
-        };
-        
-        const handleDescriptionInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setDescriptionInput(event.target.value);
-    };
-    const handleCreateToDo = () => {
-        const newToDo: ToDo = {
-          id: toDoList.length + 1,
-          title: titleInput,
-          description: descriptionInput,
-          status: 'in progress',
-        };
-        setToDoList([...toDoList, newToDo]);
-        setTitleInput('');
-        setDescriptionInput('');
-      };
+  const handleTitleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleInput(event.target.value)
+  }
+
+  const handleDescriptionInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDescriptionInput(event.target.value)
+  }
+  const handleCreateToDo = () => {
+    if (!titleInput || !descriptionInput) {
+      setError(true)
+      return
+    }
+    const newToDo: ToDo = {
+      id: toDoList.length + 1,
+      title: titleInput,
+      description: descriptionInput,
+      status: 'in progress',
+    }
+    setToDoList([...toDoList, newToDo])
+    setTitleInput('')
+    setDescriptionInput('')
+    setError(false)
+  }
+
+  const handleToDoClick = (id: number) => {
+    const clickedToDo = toDoList.find((toDo) => toDo.id === id)
+    if (clickedToDo) {
+      // handle displaying modal with to-do details
+    }
+  }
 
   return (
     <div className="App">
@@ -41,12 +54,33 @@ const ToDoList: React.FC = () => {
         </div>
         <div>
           <h5>Description:</h5>
-          <input type="text" value={descriptionInput} onChange={handleDescriptionInputChange} placeholder="enter description" required />
+          <input
+            type="text"
+            value={descriptionInput}
+            onChange={handleDescriptionInputChange}
+            placeholder="enter description"
+            required
+          />
         </div>
-        <button style={{ width: 177, height: 21, alignSelf: 'flex-end' }} onClick={handleCreateToDo}>Create</button>
+        <button style={{ width: 177, height: 21, alignSelf: 'flex-end' }} onClick={handleCreateToDo}>
+          Create
+        </button>
       </div>
+      {error && <p style={{ color: 'red' }}>This field is empty</p>}
+      <ul>
+        {toDoList.map((toDo) => (
+          <li
+            key={toDo.id}
+            onClick={() => handleToDoClick(toDo.id)}
+            style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}
+          >
+            <h3>{toDo.title}</h3>
+            <p>{toDo.description}</p>
+            <p>Status: {toDo.status}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
-export default ToDoList;
-
+export default ToDoList
